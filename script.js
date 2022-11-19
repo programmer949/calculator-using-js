7fe704eb710a7dd62f14240df34f75d3b5ebb3c3
Ayxan Əliyev <85896859+programmer949@users.noreply.github.com>
2022-11-20 00:39:41 +0400

    Add files via upload

--- script.js
+++ script.js
@@ -3,23 +3,25 @@ const display = document.querySelector(".display");
 const buttons = document.querySelectorAll("button");
 let container = "";
 
+display.innerText = "0";
+
 const clear = () => {
     display.innerText = "";
     container = "";
+    display.innerText = "0";
 };
 
 const slice = () => {
     display.innerText = display.innerText.slice(0, -1);
     container = container.slice(0, -1);
+    if (Number(display.innerText) < 1) display.innerText = "0";
 };
 
 for (let i = 0; i < buttons.length; i++) {
     body.onkeydown = (e) => {
-        if (e.key === "Delete") {
-            clear();
-        } else if (e.key === "Backspace") {
-            slice();
-        } else if (container && e.key === "Enter") {
+        if (e.key == "Delete") clear();
+        else if (e.key == "Backspace") slice();
+        else if (container && e.key == "Enter") {
             try {
                 display.innerText = eval(container);
             } catch {
@@ -28,22 +30,25 @@ for (let i = 0; i < buttons.length; i++) {
             }
         } else if (
             Number(e.key) ||
-            e.key === "0" ||
-            e.key === "+" ||
-            e.key === "-" ||
-            e.key === "*" ||
-            e.key === "/" ||
-            e.key === "." ||
-            e.key === "%"
+            e.key == "0" ||
+            e.key == "+" ||
+            e.key == "-" ||
+            e.key == "*" ||
+            e.key == "/" ||
+            e.key == "." ||
+            e.key == "%"
         ) {
-            if (buttons[i].innerText === "•") {
+            display.innerText[0] == 0
+                ? (display.innerText = display.innerText.slice(1))
+                : null;
+            if (buttons[i].innerText == "•") {
                 display.innerText += ".";
                 container += e.key;
             } else {
-                if (e.key === "*") {
+                if (e.key == "*") {
                     display.innerText += "×";
                     container += e.key;
-                } else if (e.key === "/") {
+                } else if (e.key == "/") {
                     display.innerText += "÷";
                     container += e.key;
                 } else {
@@ -53,14 +58,13 @@ for (let i = 0; i < buttons.length; i++) {
             }
         }
     };
+
     buttons[i].onclick = (e) => {
-        if (e.target.innerText === "AC") {
-            clear();
-        } else if (e.target.innerText === "C") {
-            slice();
-        } else {
+        if (e.target.innerText == "AC") clear();
+        else if (e.target.innerText == "C") slice();
+        else {
             let data = e.target.attributes.data.nodeValue;
-            if (container && data === "=") {
+            if (container && data == "=") {
                 try {
                     display.innerText = eval(container);
                 } catch {
@@ -69,15 +73,18 @@ for (let i = 0; i < buttons.length; i++) {
                 }
             } else if (
                 Number(data) ||
-                data === "0" ||
-                data === "+" ||
-                data === "-" ||
-                data === "*" ||
-                data === "/" ||
-                data === "." ||
-                data === "%"
+                data == "0" ||
+                data == "+" ||
+                data == "-" ||
+                data == "*" ||
+                data == "/" ||
+                data == "." ||
+                data == "%"
             ) {
-                if (e.target.innerText === "•") {
+                display.innerText[0] == 0
+                    ? (display.innerText = display.innerText.slice(1))
+                    : null;
+                if (e.target.innerText == "•") {
                     display.innerText += ".";
                     container += data;
                 } else {
